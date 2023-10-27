dfbe15de739b0c6a06b7776ce56fbab4882e9768
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-26 22:54:49 -0600

    revert

--- bonx-gatsby-v5/src/layouts/header/index.jsx
+++ bonx-gatsby-v5/src/layouts/header/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { SigningCosmosClient } from "@cosmjs/launchpad";
+import { StaticImage } from "gatsby-plugin-image";
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 import Logo from "../../components/logo";
 import MainMenu from "../../components/menu/main-menu";
 import MobileNavMenu from "../../components/menu/mobile-menu";
@@ -7,43 +8,57 @@ import Button from "../../components/shared/button";
 import { useSticky } from "../../hooks";
 
 const Header = ({ data }) => {
+    // Sticky Header
     const { sticky, headerRef, fixedRef } = useSticky();
+
+    // OfCanvas Menu
     const [ofcanvasOpen, setOfcanvasOpen] = useState(false);
-    const [keplrAddress, setKeplrAddress] = useState("");
 
-    async function loadKeplr() {
-        if (window.keplr) {
-            await window.keplr.experimentalSuggestChain({
-                chainId: "qwoyn-1",
-                chainName: "Qwoyn",
-                rpc: "https://rpc.qwoyn.studio",
-                rest: "https://api.qwoyn.studio",
-                stakeCurrency: {
-                    coinDenom: "QWOYN",
-                    coinMinimalDenom: "uqwoyn",
-                    coinDecimals: 6,
-                },
-            });
+    const ofcanvasHandaler = () => {
+        setOfcanvasOpen((prev) => !prev);
+    };
 
-            const offlineSigner = window.keplr.getOfflineSigner("qwoyn-1");
-            const [account] = await offlineSigner.getAccounts();
-            setKeplrAddress(account.address);
+    // Keplr state
+    const [isKeplrInstalled, setKeplrInstalled] = useState(false);
+    const [keplrAddress, setKeplrAddress] = useState(null);
 
-            const cosmosClient = new SigningCosmosClient(
-                "https://api.qwoyn.studio",
-                account.address,
-                offlineSigner
-            );
-
-            // Use `cosmosClient` as necessary for further interactions
-        } else {
-            console.error("Keplr extension not found.");
+    useEffect(() => {
+        if (window.getOfflineSigner && window.keplr) {
+            console.log("Keplr detected.");
+            setKeplrInstalled(true);
         }
-    }
+    }, []);
+
+    const loadKeplr = async () => {
+        console.log("loadKeplr function called"); // Add this
+
+        if (isKeplrInstalled) {
+            console.log("Trying to connect to Keplr"); // And this
+
+            try {
+                const chainId = "qwoyn-1"; // Replace with your chain ID
+                await window.keplr.enable(chainId);
+                const offlineSigner = window.getOfflineSigner(chainId);
+                const accounts = await offlineSigner.getAccounts();
+                setKeplrAddress(accounts[0]?.address);
+            } catch (error) {
+                console.error("Error connecting to Keplr:", error);
+            }
+        }
+    };
+
 
     return (
-        <header ref={headerRef} className="bg-transparent absolute w-full mx-auto z-40">
-            <div ref={fixedRef} className={`header-top ${sticky ? "fixed top-0 bg-secondary-100 opacity-90 w-full" : ""}`}>
+        <header
+            ref={headerRef}
+            className="bg-transparent absolute w-full mx-auto z-40"
+        >
+            <div
+                ref={fixedRef}
+                className={`header-top ${
+                    sticky ? "fixed top-0 bg-secondary-100 opacity-90 w-full" : ""
+                }`}
+            >
                 <div className="container px-4">
                     <nav className="bg-transparent flex justify-between items-center py-3">
                         <div className="text-3xl font-semibold leading-none">
@@ -51,25 +66,20 @@ const Header = ({ data }) => {
                         </div>
                         <MainMenu allmenuData={data?.menu} />
                         <div className="header-right-action flex items-center">
-                            {!keplrAddress ? (
+                            {isKeplrInstalled && !keplrAddress ? (
                                 <Button onClick={loadKeplr} shape="square2xl" className="text-white hidden xs:block">
                                     Connect Keplr
                                 </Button>
                             ) : (
-                                <span className="text-white hidden xs:block">{keplrAddress}</span>
+                                keplrAddress &&
+                                <Button shape="square2xl" className="text-white hidden xs:block">
+                                    {`qwoyn...${keplrAddress.slice(-4)}`}
+                                </Button>
                             )}
-                            <button
-                                onClick={() => setOfcanvasOpen(!ofcanvasOpen)}
-                                className="flex flex-col space-y-1.5 ml-8 lg:hidden"
-                            >
-                                <span className="line h-0.5 w-6 inline-block bg-white"></span>
-                                <span className="line h-0.5 w-6 inline-block bg-white"></span>
-                                <span className="line h-0.5 w-6 inline-block bg-white"></span>
-                            </button>
                             <MobileNavMenu
                                 MobilemenuData={data.menu}
                                 ofcanvasOpen={ofcanvasOpen}
-                                ofcanvasHandaler={() => setOfcanvasOpen(!ofcanvasOpen)}
+                                ofcanvasHandaler={ofcanvasHandaler}
                             />
                         </div>
                     </nav>
@@ -77,6 +87,12 @@ const Header = ({ data }) => {
             </div>
         </header>
     );
+};
+
+Header.propTypes = {
+    data: PropTypes.shape({
+        menu: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
 };
 
 export default Header;
