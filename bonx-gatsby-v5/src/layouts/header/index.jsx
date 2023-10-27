50a413a38340be0580808c9b209244250f6fe600
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-26 21:00:15 -0600

    testing keplr

--- bonx-gatsby-v5/src/layouts/header/index.jsx
+++ bonx-gatsby-v5/src/layouts/header/index.jsx
@@ -1,6 +1,6 @@
 import { StaticImage } from "gatsby-plugin-image";
 import PropTypes from "prop-types";
-import React, { Fragment, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../../components/logo";
 import MainMenu from "../../components/menu/main-menu";
 import MobileNavMenu from "../../components/menu/mobile-menu";
@@ -14,10 +14,30 @@ const Header = ({ data }) => {
     // OfCanvas Menu
     const [ofcanvasOpen, setOfcanvasOpen] = useState(false);
 
-    // OfCanvas Menu Open & Remove
-    const ofcanvasHandaler = () => {
-        setOfcanvasOpen((prev) => !prev);
+    // Keplr state
+    const [isKeplrInstalled, setKeplrInstalled] = useState(false);
+    const [keplrAddress, setKeplrAddress] = useState(null);
+
+    useEffect(() => {
+        if (window.getOfflineSigner && window.keplr) {
+            setKeplrInstalled(true);
+        }
+    }, []);
+
+    const loadKeplr = async () => {
+        if (isKeplrInstalled) {
+            try {
+                const chainId = "YOUR_CHAIN_ID"; // Replace with your chain ID
+                await window.keplr.enable(chainId);
+                const offlineSigner = window.getOfflineSigner(chainId);
+                const accounts = await offlineSigner.getAccounts();
+                setKeplrAddress(accounts[0]?.address);
+            } catch (error) {
+                console.error("Error connecting to Keplr:", error);
+            }
+        }
     };
+
     return (
         <header
             ref={headerRef}
@@ -26,9 +46,7 @@ const Header = ({ data }) => {
             <div
                 ref={fixedRef}
                 className={`header-top ${
-                    sticky
-                        ? "fixed top-0 bg-secondary-100 opacity-90 w-full"
-                        : ""
+                    sticky ? "fixed top-0 bg-secondary-100 opacity-90 w-full" : ""
                 }`}
             >
                 <div className="container px-4">
@@ -38,13 +56,18 @@ const Header = ({ data }) => {
                         </div>
                         <MainMenu allmenuData={data?.menu} />
                         <div className="header-right-action flex items-center">
-                            <Button
-                                path="https://explorer.theamsolutions.info/qwoyn-main/"
-                                shape="square2xl"
-                                className="text-white hidden xs:block"
-                            >
-                                Qwoyn Explorer
-                            </Button>
+                            {isKeplrInstalled && !keplrAddress ? (
+                                <Button
+                                    onClick={loadKeplr}
+                                    shape="square2xl"
+                                    className="text-white hidden xs:block"
+                                >
+                                    Connect Keplr
+                                </Button>
+                            ) : (
+                                keplrAddress && <span>Address: {keplrAddress}</span>
+                            )}
+
                             <button
                                 onClick={ofcanvasHandaler}
                                 onKeyDown={ofcanvasHandaler}
