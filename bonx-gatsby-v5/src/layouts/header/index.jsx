2415076845e2638c04a1db54b2f2ee4a95f572b8
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-26 22:37:26 -0600

    disconnect keplr

--- bonx-gatsby-v5/src/layouts/header/index.jsx
+++ bonx-gatsby-v5/src/layouts/header/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { SigningCosmosClient } from "@cosmjs/launchpad";
 import Logo from "../../components/logo";
 import MainMenu from "../../components/menu/main-menu";
 import MobileNavMenu from "../../components/menu/mobile-menu";
@@ -7,19 +8,8 @@ import { useSticky } from "../../hooks";
 
 const Header = ({ data }) => {
     const { sticky, headerRef, fixedRef } = useSticky();
-
     const [ofcanvasOpen, setOfcanvasOpen] = useState(false);
-    const [keplrAddress, setKeplrAddress] = useState(null);
-    const [balance, setBalance] = useState(null);
-
-    const ofcanvasHandler = () => {
-        setOfcanvasOpen((prev) => !prev);
-    };
-
-    const disconnectKeplr = () => {
-        setKeplrAddress(null);
-        setBalance(null);
-    };
+    const [keplrAddress, setKeplrAddress] = useState("");
 
     async function loadKeplr() {
         if (window.keplr) {
@@ -35,16 +25,17 @@ const Header = ({ data }) => {
                 },
             });
 
-            const cosmos = new window.cosmosjs.Cosmos("https://rpc.qwoyn.studio", "qwoyn-1");
-            cosmos.setBech32MainPrefix("qwoyn"); // Note: Update this if the prefix is different for your chain
-            cosmos.setPath("m/44'/118'/0'/0/0");
-            const offlineSigner = window.getOfflineSigner("qwoyn-1");
-            cosmos.setOfflineSigner(offlineSigner);
-
+            const offlineSigner = window.keplr.getOfflineSigner("qwoyn-1");
             const [account] = await offlineSigner.getAccounts();
             setKeplrAddress(account.address);
 
-            // You can expand on this for fetching and setting the balance, as well as other functionalities.
+            const cosmosClient = new SigningCosmosClient(
+                "https://api.qwoyn.studio",
+                account.address,
+                offlineSigner
+            );
+
+            // Use `cosmosClient` as necessary for further interactions
         } else {
             console.error("Keplr extension not found.");
         }
@@ -60,29 +51,26 @@ const Header = ({ data }) => {
                         </div>
                         <MainMenu allmenuData={data?.menu} />
                         <div className="header-right-action flex items-center">
-                            {keplrAddress ? (
-                                <div className="hover-content">
-                                    <Button onClick={disconnectKeplr} shape="square2xl" className="text-white hidden xs:block">
-                                        {keplrAddress.substr(0, 5) + "..." + keplrAddress.substr(-4)}
-                                    </Button>
-                                    <div className="hover-dropdown">
-                                        Balance: {balance}
-                                        <Button onClick={disconnectKeplr}>
-                                            Disconnect
-                                        </Button>
-                                    </div>
-                                </div>
-                            ) : (
+                            {!keplrAddress ? (
                                 <Button onClick={loadKeplr} shape="square2xl" className="text-white hidden xs:block">
                                     Connect Keplr
                                 </Button>
+                            ) : (
+                                <span className="text-white hidden xs:block">{keplrAddress}</span>
                             )}
-                            <button onClick={ofcanvasHandler} onKeyDown={ofcanvasHandler} className="flex flex-col space-y-1.5 ml-8 lg:hidden">
+                            <button
+                                onClick={() => setOfcanvasOpen(!ofcanvasOpen)}
+                                className="flex flex-col space-y-1.5 ml-8 lg:hidden"
+                            >
                                 <span className="line h-0.5 w-6 inline-block bg-white"></span>
                                 <span className="line h-0.5 w-6 inline-block bg-white"></span>
                                 <span className="line h-0.5 w-6 inline-block bg-white"></span>
                             </button>
-                            <MobileNavMenu MobilemenuData={data.menu} ofcanvasOpen={ofcanvasOpen} ofcanvasHandaler={ofcanvasHandler} />
+                            <MobileNavMenu
+                                MobilemenuData={data.menu}
+                                ofcanvasOpen={ofcanvasOpen}
+                                ofcanvasHandaler={() => setOfcanvasOpen(!ofcanvasOpen)}
+                            />
                         </div>
                     </nav>
                 </div>
