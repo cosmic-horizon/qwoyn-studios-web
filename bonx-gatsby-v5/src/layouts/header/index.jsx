8a48db5bccd7bf12a802e65c5292cd2b7aeaa65f
Daniel Pittman <dpdanpittman@gmail.com>
2023-10-26 22:19:06 -0600

    disconnect keplr

--- bonx-gatsby-v5/src/layouts/header/index.jsx
+++ bonx-gatsby-v5/src/layouts/header/index.jsx
@@ -1,6 +1,4 @@
-import { StaticImage } from "gatsby-plugin-image";
-import PropTypes from "prop-types";
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Logo from "../../components/logo";
 import MainMenu from "../../components/menu/main-menu";
 import MobileNavMenu from "../../components/menu/mobile-menu";
@@ -8,57 +6,41 @@ import Button from "../../components/shared/button";
 import { useSticky } from "../../hooks";
 
 const Header = ({ data }) => {
-    // Sticky Header
     const { sticky, headerRef, fixedRef } = useSticky();
-
-    // OfCanvas Menu
     const [ofcanvasOpen, setOfcanvasOpen] = useState(false);
+    const [keplrAddress, setKeplrAddress] = useState("");
+    const [balance, setBalance] = useState(0);
+    const [hoveringButton, setHoveringButton] = useState(false);
+
+    const loadKeplr = async () => {
+        if (window.keplr) {
+            const address = await window.keplr.getAddress();
+            setKeplrAddress(address);
+        }
+    };
+
+    const disconnectKeplr = () => {
+        setKeplrAddress("");
+    };
+
+    useEffect(() => {
+        if (window.keplr && keplrAddress) {
+            // Replace this with the actual method to get balance from Keplr
+            window.keplr.getBalance(keplrAddress).then((bal) => {
+                setBalance(bal);
+            });
+        }
+    }, [keplrAddress]);
+
+    const truncatedAddress = `${keplrAddress.slice(0, 5)}...${keplrAddress.slice(-4)}`;
 
     const ofcanvasHandaler = () => {
         setOfcanvasOpen((prev) => !prev);
     };
 
-    // Keplr state
-    const [isKeplrInstalled, setKeplrInstalled] = useState(false);
-    const [keplrAddress, setKeplrAddress] = useState(null);
-
-    useEffect(() => {
-        if (window.getOfflineSigner && window.keplr) {
-            console.log("Keplr detected.");
-            setKeplrInstalled(true);
-        }
-    }, []);
-
-    const loadKeplr = async () => {
-        console.log("loadKeplr function called"); // Add this
-
-        if (isKeplrInstalled) {
-            console.log("Trying to connect to Keplr"); // And this
-
-            try {
-                const chainId = "qwoyn-1"; // Replace with your chain ID
-                await window.keplr.enable(chainId);
-                const offlineSigner = window.getOfflineSigner(chainId);
-                const accounts = await offlineSigner.getAccounts();
-                setKeplrAddress(accounts[0]?.address);
-            } catch (error) {
-                console.error("Error connecting to Keplr:", error);
-            }
-        }
-    };
-
-
     return (
-        <header
-            ref={headerRef}
-            className="bg-transparent absolute w-full mx-auto z-40"
-        >
-            <div
-                ref={fixedRef}
-                className={`header-top ${
-                    sticky ? "fixed top-0 bg-secondary-100 opacity-90 w-full" : ""
-                }`}
-            >
+        <header ref={headerRef} className="bg-transparent absolute w-full mx-auto z-40">
+            <div ref={fixedRef} className={`header-top ${sticky ? "fixed top-0 bg-secondary-100 opacity-90 w-full" : ""}`}>
                 <div className="container px-4">
                     <nav className="bg-transparent flex justify-between items-center py-3">
                         <div className="text-3xl font-semibold leading-none">
@@ -66,16 +48,30 @@ const Header = ({ data }) => {
                         </div>
                         <MainMenu allmenuData={data?.menu} />
                         <div className="header-right-action flex items-center">
-                            {isKeplrInstalled && !keplrAddress ? (
-                                <Button onClick={loadKeplr} shape="square2xl" className="text-white hidden xs:block">
-                                    Connect Keplr
-                                </Button>
-                            ) : (
-                                keplrAddress &&
-                                <Button shape="square2xl" className="text-white hidden xs:block">
-                                    {`qwoyn...${keplrAddress.slice(-4)}`}
-                                </Button>
-                            )}
+                            <Button
+                                shape="square2xl"
+                                className="text-white hidden xs:block relative"
+                                onClick={keplrAddress ? null : loadKeplr}
+                                onMouseEnter={() => setHoveringButton(true)}
+                                onMouseLeave={() => setHoveringButton(false)}
+                            >
+                                {keplrAddress ? truncatedAddress : "Connect Keplr"}
+                                {hoveringButton && keplrAddress && (
+                                    <div className="absolute bg-white text-black p-2 mt-1">
+                                        Balance: {balance}
+                                        <button onClick={disconnectKeplr}>Disconnect</button>
+                                    </div>
+                                )}
+                            </Button>
+                            <button
+                                onClick={ofcanvasHandaler}
+                                onKeyDown={ofcanvasHandaler}
+                                className="flex flex-col space-y-1.5 ml-8 lg:hidden"
+                            >
+                                <span className="line h-0.5 w-6 inline-block bg-white"></span>
+                                <span className="line h-0.5 w-6 inline-block bg-white"></span>
+                                <span className="line h-0.5 w-6 inline-block bg-white"></span>
+                            </button>
                             <MobileNavMenu
                                 MobilemenuData={data.menu}
                                 ofcanvasOpen={ofcanvasOpen}
@@ -87,12 +83,6 @@ const Header = ({ data }) => {
             </div>
         </header>
     );
-};
-
-Header.propTypes = {
-    data: PropTypes.shape({
-        menu: PropTypes.arrayOf(PropTypes.shape({})),
-    }),
 };
 
 export default Header;
